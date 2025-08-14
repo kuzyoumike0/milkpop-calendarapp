7ea40ffdc518ad5e534dc9d03b223215fc5c0f3a
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-14 23:28:27 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SharedCalendar from './components/SharedCalendar';
 import PersonalCalendar from './components/PersonalCalendar';
+import './App.css';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Link to="/">共有カレンダー</Link>
         <Link to="/personal">個人カレンダー</Link>
       </div>
+
       <Routes>
         <Route path="/" element={<SharedCalendar />} />
         <Route path="/personal" element={<PersonalCalendar />} />
