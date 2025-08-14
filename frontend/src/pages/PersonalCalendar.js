450ce7f731ce6d381bba3ba554719da7e618bd03
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-15 07:52:29 +0900

    Update PersonalCalendar.js

--- frontend/src/pages/PersonalCalendar.js
+++ frontend/src/pages/PersonalCalendar.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function SharedCalendar() {
+export default function PersonalCalendar() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/shared?date=2025-08-15")
+    axios.get("/api/personal/1?date=2025-08-15")
       .then(res => setEvents(res.data))
       .catch(err => console.error(err));
   }, []);
 
   return (
     <div>
-      <h2>共有カレンダー</h2>
+      <h2>個人カレンダー</h2>
       <ul>
         {events.map(e => (
           <li key={e.id}>{e.time_slot} - {e.title}</li>
