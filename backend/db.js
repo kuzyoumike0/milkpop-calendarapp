48758c197a0409c8e0b8026c01aa9071b0350b0e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 07:12:17 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER || 'postgres',
-    host: process.env.POSTGRES_HOST || 'db',
-    database: process.env.POSTGRES_DB || 'milkpop_calendar',
-    password: process.env.POSTGRES_PASSWORD || 'postgres',
-    port: 5432
+  connectionString: 'postgresql://postgres:eJjJplyhHrsYWyTqeOauZwunjRPMFUFv@tramway.proxy.rlwy.net:39592/railway'
 });
 
 module.exports = pool;
