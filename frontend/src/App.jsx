e545b56753dea0d764255a094ce58bac71dcc8ce
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-15 13:15:29 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SharedCalendar from "./pages/SharedCalendar";
@@ -7,9 +6,9 @@ import PersonalCalendar from "./pages/PersonalCalendar";
 export default function App() {
   return (
     <Router>
-      <nav className="text-center my-6 space-x-6">
-        <Link to="/" className="text-indigo-600 font-semibold hover:underline">共有カレンダー</Link>
-        <Link to="/personal" className="text-indigo-600 font-semibold hover:underline">個人カレンダー</Link>
+      <nav className="text-center my-4">
+        <Link to="/" className="mr-4 text-indigo-600 font-medium">共有カレンダー</Link>
+        <Link to="/personal" className="text-indigo-600 font-medium">個人カレンダー</Link>
       </nav>
 
       <Routes>
