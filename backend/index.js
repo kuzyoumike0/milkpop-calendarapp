f242dc8293dd6fa2bd51ddc2e20a1eeac56a3e1b
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 08:57:41 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,19 @@
-// backend/index.js
-const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const eventsRouter = require('./routes/events');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-// APIルート
 app.use('/api/events', eventsRouter);
 
-// React ビルド配信
-// 修正: frontend/build を指定
+// フロントビルド配信
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
