f039c31bbd5d14b24a52d889893d2b5bbdf0f14e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 05:26:04 +0900

    Delete frontend directory

--- frontend/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' });
-
-export const register = (data) => API.post('/users/register', data);
-export const login = (data) => API.post('/users/login', data);
-export const getEvents = (token) => API.get('/events', { headers: { Authorization: 'Bearer '+token } });
-export const createEvent = (data, token) => API.post('/events', data, { headers: { Authorization: 'Bearer '+token } });
