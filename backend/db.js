e22e9f6b681554e436dbe0d018520925eeae6a9e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 07:30:25 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,7 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:eJjJplyhHrsYWyTqeOauZwunjRPMFUFv@tramway.proxy.rlwy.net:39592/railway',
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL
 });
 
-pool.query('SELECT NOW()')
-  .then(res => console.log(res.rows))
-  .catch(err => console.error(err));
+module.exports = pool;
