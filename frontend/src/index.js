3ad5c507e32593733a255fe6cf4d90f76060975b
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-15 13:37:58 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Tailwind CSS を利用する場合
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
