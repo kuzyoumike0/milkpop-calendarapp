d0ef279012ca70824d57f8b3c9ec86d606350141
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 07:54:04 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
