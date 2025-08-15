fb932daf8287b19670185d4e3f840bbd0fac5728
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-15 13:15:11 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,7 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // TailwindCSS を読み込む
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
