d8d7d900320e2f24b2d1ab93b862c414549ad2a7
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-15 13:29:35 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <Router>
       <nav className="text-center my-4">
-        <Link to="/" className="mr-4 text-indigo-600 font-medium">共有カレンダー</Link>
-        <Link to="/personal" className="text-indigo-600 font-medium">個人カレンダー</Link>
+        <Link to="/" className="mr-4 text-indigo-600 font-bold">共有カレンダー</Link>
+        <Link to="/personal" className="text-indigo-600 font-bold">個人カレンダー</Link>
       </nav>
 
       <Routes>
