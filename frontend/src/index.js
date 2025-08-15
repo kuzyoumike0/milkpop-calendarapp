93438a0016794a1aa4086068f2862914950fcb5c
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-15 23:14:09 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Tailwind CSS を利用する場合
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
