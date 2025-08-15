ead8b63a4159bb828aec046e0682bd534494f842
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 08:30:03 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
