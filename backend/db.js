f5e1f53526b5beb7e07616180b3f6b2b233b744b
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 08:12:28 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL, // Railway の接続URL
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
