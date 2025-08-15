e07ef46c019352a31b86e2c8379aac60e5cded34
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 05:35:24 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,14 @@ function App() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/events")
+    axios.get("/api/events")
       .then(res => setEvents(res.data))
       .catch(err => console.error(err));
   }, []);
 
   return (
     <div>
-      <h1>カレンダー</h1>
+      <h1>Calendar App</h1>
       <ul>
         {events.map(e => (
           <li key={e.id}>{e.title} - {e.date}</li>
