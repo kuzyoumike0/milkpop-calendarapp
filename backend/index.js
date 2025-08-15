d85ad66c0de126fd1e8337e834bef8b3cdaec55e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 08:26:50 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// APIルート
 app.use('/api/events', eventsRouter);
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// React ビルド配信（Docker内でビルド済みのパスに変更）
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
