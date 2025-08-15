36438ab00ba830ceec37421cc0bbb7b2e53d8f60
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 08:19:03 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// APIルート
 app.use('/api/events', eventsRouter);
 
-// React ビルド配信
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
