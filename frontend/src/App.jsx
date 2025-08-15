52ef38b6ac600d2ac6181bfda5a61dd04a4cc735
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-15 13:08:38 +0900

    Update and rename App.js to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SharedCalendar from "./pages/SharedCalendar";
@@ -6,9 +7,9 @@ import PersonalCalendar from "./pages/PersonalCalendar";
 export default function App() {
   return (
     <Router>
-      <nav style={{ textAlign: "center", margin: "20px" }}>
-        <Link to="/" style={{ marginRight: "20px" }}>共有カレンダー</Link>
-        <Link to="/personal">個人カレンダー</Link>
+      <nav className="text-center my-6 space-x-6">
+        <Link to="/" className="text-indigo-600 font-semibold hover:underline">共有カレンダー</Link>
+        <Link to="/personal" className="text-indigo-600 font-semibold hover:underline">個人カレンダー</Link>
       </nav>
 
       <Routes>
