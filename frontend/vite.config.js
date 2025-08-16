b9422c36a87cd14ea8d9f11df0d7e1e19b4cecdf
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 09:11:46 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
+  root: './frontend',
   build: {
-    outDir: 'build',
+    outDir: 'dist',
+    emptyOutDir: true
   },
   resolve: {
     alias: {
-      '@': '/src',
-    },
-  },
+      '@': path.resolve(__dirname, './frontend/src')
+    }
+  }
 });
