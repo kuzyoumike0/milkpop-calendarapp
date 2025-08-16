7d6b24743bc77a411b8ba4c308c39211512208e3
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 09:30:29 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Railway など外部DBでもOK
+  user: process.env.POSTGRES_USER || 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  database: process.env.POSTGRES_DB || 'railway',
+  password: process.env.POSTGRES_PASSWORD || '',
+  port: process.env.POSTGRES_PORT || 5432,
 });
 
 module.exports = pool;
