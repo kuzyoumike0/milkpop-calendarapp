828a36a8ca0d33ba91d3908d1ca3996e121596f6
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 18:36:19 +0900

    Delete frontend directory

--- frontend/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold">Milkpop Calendar App</h1>
-    </div>
-  );
-}
