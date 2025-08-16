d1c209e3a189bf2991a039ada4efb187383470c4
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-16 09:07:44 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Railway で設定される環境変数
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
