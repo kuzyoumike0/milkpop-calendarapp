7f5ccf14ba4a3ba009c0b994dbd38891ad4a7887
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-17 22:19:53 +0900

    Delete frontend directory

--- frontend/src/pages/PersonalSchedule.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function PersonalSchedule() {
-  return <div className="p-6">👤 個人スケジュール画面</div>;
-}
--- frontend/src/pages/SharedCalendar.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SharedCalendar() {
-  return <div className="p-6">🌍 共有カレンダー画面</div>;
-}
