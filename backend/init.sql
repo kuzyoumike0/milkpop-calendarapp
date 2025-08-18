4edee0969b0f38ed10ca6df2d738d3d08964b25a
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 08:59:49 +0900

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -1,6 +1,13 @@
+CREATE TABLE IF NOT EXISTS shared_links (
+  id TEXT PRIMARY KEY,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
 CREATE TABLE IF NOT EXISTS shared_schedules (
   id SERIAL PRIMARY KEY,
+  link_id TEXT NOT NULL REFERENCES shared_links(id) ON DELETE CASCADE,
   date DATE NOT NULL,
   title TEXT NOT NULL,
-  username TEXT NOT NULL  -- 作成者
+  username TEXT NOT NULL,
+  time_info TEXT NOT NULL
 );
