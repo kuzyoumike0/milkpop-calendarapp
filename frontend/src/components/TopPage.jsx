b39f5882cf8b23e41e35425a5032a11da49d9280
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 08:35:35 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function TopPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>トップページ</h1>
+      <p>個人用ページや共有ページに移動できます。</p>
+    </div>
+  );
+}
+import React from "react";
+
+export default function TopPage() {
   return <h1>トップページ</h1>;
 }
