0912f65b5a5b904d407e3cddbfe6c6452662ca3f
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 08:18:16 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import CalendarPage from "./components/CalendarPage";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import TopPage from "./components/TopPage";
+import PersonalPage from "./components/PersonalPage";
+import SharePage from "./components/SharePage";
 
 export default function App() {
   return (
-    <div>
-      <h1>共有カレンダー</h1>
-      <nav>
-        <Link to="/">ホーム</Link> | <Link to="/calendar">カレンダー</Link>
+    <Router>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/">トップ</Link> | <Link to="/personal">個人</Link> |{" "}
+        <Link to="/share">共有</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<div>トップページです</div>} />
-        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/" element={<TopPage />} />
+        <Route path="/personal" element={<PersonalPage />} />
+        <Route path="/share" element={<SharePage />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
