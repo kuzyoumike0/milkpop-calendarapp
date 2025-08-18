ed130421ed30c6337f4f162f1da33d6d48db9312
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 08:47:06 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,6 +2,14 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   webpack: {
+    configure: (webpackConfig) => {
+      // console ポリフィルを無効化
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        console: false,
+      };
+      return webpackConfig;
+    },
     plugins: {
       add: [
         new NodePolyfillPlugin()
