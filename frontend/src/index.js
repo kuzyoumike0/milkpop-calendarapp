28557a48a5c87c5eda236a9f96456687d67b8e1a
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-18 19:21:39 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8" />
-    <title>共有カレンダー</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-</html>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
