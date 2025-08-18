c0cb4b3db1e6d93eed8a01a833ed4b38e6a49aae
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-18 13:35:29 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ShareButton from "./components/ShareButton";
-import PersonalPage from "./PersonalPage";
-import SharedPage from "./SharedPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TopPage from "./TopPage";
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <div style={{ padding: "20px" }}>
-        <h1>📅 カレンダーアプリ</h1>
-
-        {/* ナビゲーション */}
-        <nav style={{ marginBottom: "20px" }}>
-          <Link to="/" style={{ marginRight: "15px" }}>トップページ</Link>
-          <Link to="/personal" style={{ marginRight: "15px" }}>個人スケジュール</Link>
-          <Link to="/shared">共有カレンダー</Link>
-        </nav>
-
-        <Routes>
-          {/* トップページ */}
-          <Route
-            path="/"
-            element={
-              <div>
-                <h2>トップページ</h2>
-                <p>ここから各ページに移動できます。</p>
-                <ShareButton />
-              </div>
-            }
-          />
-
-          {/* 個人スケジュール */}
-          <Route path="/personal" element={<PersonalPage />} />
-
-          {/* 共有カレンダー */}
-          <Route path="/shared" element={<SharedPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+      </Routes>
     </Router>
   );
 }
-
-export default App;
