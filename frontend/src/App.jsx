7cb466324675c7858269fbbcaf01103f02098bb9
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 07:08:29 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// ここを正しいファイル名に合わせる（CalendarPage は使いません）
 import TopPage from "./components/TopPage";
 import SharePage from "./components/SharePage";
 import ShareLinkPage from "./components/ShareLinkPage";
@@ -17,7 +16,6 @@ export default function App() {
         <Route path="/share/:id" element={<ShareLinkPage />} />
         <Route path="/personal" element={<PersonalPage />} />
         <Route path="/link/:id" element={<LinkPage />} />
-        {/* どのパスにも当てはまらない場合はトップへ */}
         <Route path="*" element={<TopPage />} />
       </Routes>
     </Router>
