24b263d1585249db4807b87b758b19e1b273a096
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 08:19:15 +0900

    Update LinkPage.jsx

--- frontend/src/components/LinkPage.jsx
+++ frontend/src/components/LinkPage.jsx
@@ -1,13 +1,5 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
 
 export default function LinkPage() {
-  const { id } = useParams();
-  return (
-    <div style={{padding:"24px"}}>
-      <h2>🔗 共有リンク</h2>
-      <p>リンクID: <b>{id}</b></p>
-      <p><Link to={`/share/${id}`}>共有リンク先ページへ</Link></p>
-    </div>
-  );
+  return <div>リンクページ</div>;
 }
