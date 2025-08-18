ff479e6399445e5afbaaff649fcc2faf1a5ea6ed
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 08:29:24 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,18 +3,9 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = {
   webpack: {
     plugins: {
-      add: [new NodePolyfillPlugin()]
+      add: [
+        new NodePolyfillPlugin()
+      ],
     },
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.fallback = {
-        fs: false,
-        path: require.resolve("path-browserify"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        os: require.resolve("os-browserify/browser"),
-        buffer: require.resolve("buffer/")
-      };
-      return webpackConfig;
-    }
-  }
+  },
 };
