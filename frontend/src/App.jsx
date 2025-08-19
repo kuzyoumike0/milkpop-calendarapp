9bd4063ba5f7116b68e0c7c73a8b83086374cb60
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 07:10:45 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,31 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import PersonalPage from "./components/PersonalPage";
-import RegisterPage from "./components/RegisterPage";
+import ShareLinkPage from "./components/ShareLinkPage";
 import SharePage from "./components/SharePage";
 
 export default function App() {
   return (
     <Router>
-      <nav style={{ padding: "10px", background: "#eee" }}>
-        <Link to="/">トップ</Link> |{" "}
-        <Link to="/register">日程登録</Link> |{" "}
-        <Link to="/personal">個人日程登録</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/personal" element={<PersonalPage />} />
-        <Route path="/share/:linkid" element={<SharePage />} />
-      </Routes>
+      <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+        <h1>📅 スケジュール共有アプリ</h1>
+
+        {/* ナビゲーション */}
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/" style={{ marginRight: "10px" }}>トップページ</Link>
+          <Link to="/register" style={{ marginRight: "10px" }}>日程登録ページ</Link>
+          <Link to="/personal" style={{ marginRight: "10px" }}>個人日程登録ページ</Link>
+          <Link to="/share/sample" style={{ marginRight: "10px" }}>共有ページ(サンプル)</Link>
+        </nav>
+
+        {/* ページ遷移 */}
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/register" element={<ShareLinkPage />} />
+          <Route path="/personal" element={<PersonalPage />} />
+          <Route path="/share/:linkId" element={<SharePage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
