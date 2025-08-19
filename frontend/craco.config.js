c3c1bd3f4627135608580845d8673995abaf5ce0
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 07:27:52 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -5,9 +5,6 @@ module.exports = {
         ...webpackConfig.resolve.fallback,
         fs: false,
         path: require.resolve("path-browserify"),
-        os: require.resolve("os-browserify/browser"),
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
       };
       return webpackConfig;
     },
