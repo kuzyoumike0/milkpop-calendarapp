99b7da1c90a0eeaa3c2df0cb55460262501bace0
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 06:11:17 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import ShareLinkPage from "./components/ShareLinkPage";
-import SharePage from "./components/SharePage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TopPage />} />
-        <Route path="/link" element={<ShareLinkPage />} />
-        <Route path="/share/:linkId" element={<SharePage />} />
+        <Route path="/links/:linkId" element={<ShareLinkPage />} />
       </Routes>
     </Router>
   );
