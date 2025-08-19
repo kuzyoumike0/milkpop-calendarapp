6bc6a2c044a021df3c16bed561ef6495db834056
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 05:54:35 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,7 +3,20 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = {
   webpack: {
     plugins: {
-      add: [new NodePolyfillPlugin()]
+      add: [
+        new NodePolyfillPlugin({
+          excludeAliases: ["console"] // ← これで console-browserify を無効化
+        })
+      ]
+    },
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        fs: false,
+        path: require.resolve("path-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        stream: require.resolve("stream-browserify")
+      };
+      return webpackConfig;
     }
   }
 };
