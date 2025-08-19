eafb3bcc5405e698a389ce2428c47e3863f7a964
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 06:48:37 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// ページコンポーネント
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import PersonalPage from "./components/PersonalPage";
+import RegisterPage from "./components/RegisterPage";
 import SharePage from "./components/SharePage";
-import ShareLinkPage from "./components/ShareLinkPage";
-import LinkPage from "./components/LinkPage";
 
 export default function App() {
   return (
     <Router>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/">トップ</Link> |{" "}
+        <Link to="/register">日程登録</Link> |{" "}
+        <Link to="/personal">個人日程登録</Link>
+      </nav>
       <Routes>
-        {/* トップページ */}
         <Route path="/" element={<TopPage />} />
-
-        {/* 個人スケジュール入力ページ */}
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/personal" element={<PersonalPage />} />
-
-        {/* 共有リンク発行ページ */}
-        <Route path="/share" element={<SharePage />} />
-
-        {/* 共有リンクページ（発行したリンク先） */}
-        <Route path="/links/:linkId" element={<ShareLinkPage />} />
-
-        {/* 汎用リンクページ（旧仕様対応など） */}
-        <Route path="/link" element={<LinkPage />} />
-
-        {/* 存在しないURL → トップに戻す */}
-        <Route path="*" element={<TopPage />} />
+        <Route path="/share/:linkid" element={<SharePage />} />
       </Routes>
     </Router>
   );
