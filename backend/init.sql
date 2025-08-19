4ac43f33e38a7d3a6fc29370273d0f3e852b2221
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 09:44:36 +0900

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS schedules;
 
 CREATE TABLE schedules (
-  id SERIAL PRIMARY KEY,
-  date DATE NOT NULL,
-  username TEXT,
-  category TEXT,
-  starttime TEXT,
-  endtime TEXT,
-  linkid TEXT
+    id TEXT PRIMARY KEY,           -- UUID
+    username TEXT NOT NULL,        -- 投稿者
+    date DATE NOT NULL,            -- 日付
+    category TEXT NOT NULL,        -- 区分 (終日/昼/夜/時間帯)
+    startTime TEXT,                -- 開始時刻 (時間帯指定時のみ)
+    endTime TEXT,                  -- 終了時刻 (時間帯指定時のみ)
+    linkId TEXT NOT NULL           -- 共有リンクの識別子
 );
