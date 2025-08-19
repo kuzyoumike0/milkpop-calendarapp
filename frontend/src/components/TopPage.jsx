b085075a64151931b1b570e92486b47bec3d4970
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 07:23:12 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -4,17 +4,9 @@ import { Link } from "react-router-dom";
 export default function TopPage() {
   return (
     <div>
-      <h2>🏠 トップページ</h2>
-      <p>このアプリでは、みんなで日程を共有できます。</p>
-
-      <div style={{ marginTop: "20px" }}>
-        <Link to="/register" style={{ marginRight: "15px" }}>
-          📌 日程登録ページへ
-        </Link>
-        <Link to="/personal">
-          ✍ 個人日程登録ページへ
-        </Link>
-      </div>
+      <h1>トップページ</h1>
+      <Link to="/link">日程登録ページへ</Link><br/>
+      <Link to="/personal">個人日程登録ページへ</Link>
     </div>
   );
 }
