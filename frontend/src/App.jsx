ba44aa856ab232716f89cef71ebf59ddd9124a18
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 07:23:01 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import PersonalPage from "./components/PersonalPage";
-import ShareLinkPage from "./components/ShareLinkPage";
+import LinkPage from "./components/LinkPage";
 import SharePage from "./components/SharePage";
+import ShareLinkPage from "./components/ShareLinkPage";
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
-        <h1>📅 スケジュール共有アプリ</h1>
-
-        {/* ナビゲーション */}
-        <nav style={{ marginBottom: "20px" }}>
-          <Link to="/" style={{ marginRight: "10px" }}>トップページ</Link>
-          <Link to="/register" style={{ marginRight: "10px" }}>日程登録ページ</Link>
-          <Link to="/personal" style={{ marginRight: "10px" }}>個人日程登録ページ</Link>
-          <Link to="/share/sample" style={{ marginRight: "10px" }}>共有ページ(サンプル)</Link>
-        </nav>
-
-        {/* ページ遷移 */}
-        <Routes>
-          <Route path="/" element={<TopPage />} />
-          <Route path="/register" element={<ShareLinkPage />} />
-          <Route path="/personal" element={<PersonalPage />} />
-          <Route path="/share/:linkId" element={<SharePage />} />
-        </Routes>
-      </div>
+      <nav>
+        <Link to="/">トップ</Link> | 
+        <Link to="/link">日程登録</Link> | 
+        <Link to="/personal">個人日程</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/link" element={<LinkPage />} />
+        <Route path="/personal" element={<PersonalPage />} />
+        <Route path="/share" element={<SharePage />} />
+        <Route path="/sharelink/:id" element={<ShareLinkPage />} />
+      </Routes>
     </Router>
   );
 }
+
+export default App;
