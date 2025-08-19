2093ac818e8178a521c2be11253e13658ae1d761
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 08:11:28 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function TopPage() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>ようこそ！</h2>
-      <p>このアプリで日程を登録・共有できます。</p>
-      <Link to="/register">日程登録ページへ</Link><br />
-      <Link to="/personal">個人日程登録ページへ</Link>
+    <div className="text-center space-y-6">
+      <h2 className="text-3xl">ようこそ MilkPOP Calendar</h2>
+      <div className="space-x-6">
+        <Link className="bg-[#004CA0] text-white px-4 py-2 rounded" to="/link">日程登録</Link>
+        <Link className="bg-[#FDB9C8] text-black px-4 py-2 rounded" to="/personal">個人スケジュール</Link>
+      </div>
     </div>
   );
 }
