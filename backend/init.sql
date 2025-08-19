27348f62e8981f3ee421545f4ee16423859f6993
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 16:17:45 +0900

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS schedules (
   id SERIAL PRIMARY KEY,
-  linkId TEXT NOT NULL,
+  linkid TEXT NOT NULL,
   username TEXT NOT NULL,
-  date TEXT NOT NULL,
-  timeslot TEXT NOT NULL,
-  UNIQUE(linkId, username, date, timeslot)
+  date DATE NOT NULL,
+  timeslot TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS links (
-  linkId TEXT PRIMARY KEY,
-  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  linkid TEXT PRIMARY KEY,
+  created_at TIMESTAMP DEFAULT NOW()
 );
