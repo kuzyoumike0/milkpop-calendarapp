664f273af97e34a4019aa0a83e832c21ae4c0a41
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 08:11:16 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TopPage from "./components/TopPage";
-import PersonalPage from "./components/PersonalPage";
 import LinkPage from "./components/LinkPage";
+import PersonalPage from "./components/PersonalPage";
 import SharePage from "./components/SharePage";
 import ShareLinkPage from "./components/ShareLinkPage";
-
-const Navbar = () => (
-  <div style={{ background: "#004CA0", padding: "10px", color: "white" }}>
-    <h1 style={{ display: "inline", marginRight: "20px", color: "#FDB9C8" }}>MilkPOP Calendar</h1>
-    <Link to="/" style={{ marginRight: "15px", color: "white" }}>トップ</Link>
-    <Link to="/register" style={{ marginRight: "15px", color: "white" }}>日程登録</Link>
-    <Link to="/personal" style={{ marginRight: "15px", color: "white" }}>個人日程</Link>
-  </div>
-);
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-        <Route path="/register" element={<LinkPage />} />
-        <Route path="/personal" element={<PersonalPage />} />
-        <Route path="/share/:linkid" element={<SharePage />} />
-        <Route path="/links" element={<ShareLinkPage />} />
-      </Routes>
+      <div className="bg-black text-white min-h-screen">
+        <header className="bg-[#FDB9C8] text-black p-4 flex justify-between">
+          <h1 className="font-bold text-xl">MilkPOP Calendar</h1>
+          <nav className="space-x-4">
+            <Link to="/">トップ</Link>
+            <Link to="/link">日程登録</Link>
+            <Link to="/personal">個人スケジュール</Link>
+          </nav>
+        </header>
+        <main className="p-4">
+          <Routes>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/link" element={<LinkPage />} />
+            <Route path="/personal" element={<PersonalPage />} />
+            <Route path="/share/:linkid" element={<SharePage />} />
+            <Route path="/sharelink" element={<ShareLinkPage />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
