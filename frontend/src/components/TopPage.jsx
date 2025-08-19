1d5aaf152b9f3ad0b9abdea608881214c6e21d3d
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 07:40:36 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function TopPage() {
   return (
-    <div>
-      <h2>トップページ</h2>
-      <p>
-        <Link to="/link">日程登録ページへ</Link>
-      </p>
-      <p>
-        <Link to="/personal">個人スケジュールページへ</Link>
-      </p>
+    <div className="text-center">
+      <h2 className="text-3xl font-bold text-[#004CA0]">ようこそ！</h2>
+      <p className="mt-2">日程を登録したり、個人スケジュールを管理できます。</p>
+      <div className="mt-6 space-x-4">
+        <Link to="/share" className="bg-[#FDB9C8] px-4 py-2 rounded-xl">日程登録ページへ</Link>
+        <Link to="/personal" className="bg-[#004CA0] text-white px-4 py-2 rounded-xl">個人スケジュールへ</Link>
+      </div>
     </div>
   );
 }
