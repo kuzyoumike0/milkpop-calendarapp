30d9a5748004d1954a6933fb44ae302a02af49dc
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 07:41:01 +0900

    Update ShareButton.jsx

--- frontend/src/components/ShareButton.jsx
+++ frontend/src/components/ShareButton.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function ShareButton({ onClick }) {
-  return <button onClick={onClick}>共有リンク作成</button>;
+export default function ShareButton({ link }) {
+  return (
+    <div className="mt-4">
+      <p>共有リンク:</p>
+      <a href={link} className="text-[#004CA0] underline">{link}</a>
+    </div>
+  );
 }
