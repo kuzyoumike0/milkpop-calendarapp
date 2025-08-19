31977368028bc574b2f6e753c8cdc4526afd5a3c
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 11:43:09 +0900

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -1,4 +1,7 @@
--- schedules テーブル作成
+-- 古いテーブルが存在する場合は削除
+DROP TABLE IF EXISTS schedule;
+
+-- 新しい schedules テーブルを作成
 CREATE TABLE IF NOT EXISTS schedules (
   id SERIAL PRIMARY KEY,
   username TEXT NOT NULL,
