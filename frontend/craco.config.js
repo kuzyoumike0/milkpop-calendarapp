31a95c40513c1f6f383a4f9019601f8cca251f04
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 05:37:57 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,21 +2,8 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      // 不要な console ポリフィルを無効化
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        console: false,
-      };
-      return webpackConfig;
-    },
     plugins: {
-      add: [
-        new NodePolyfillPlugin({
-          // console を除外し、それ以外の Node.js 標準ライブラリはブラウザで使えるようにする
-          excludeAliases: ["console"],
-        }),
-      ],
-    },
-  },
+      add: [new NodePolyfillPlugin()]
+    }
+  }
 };
