30b2d9ab306c548a39d4fe2d13f1834919f2c130
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 07:41:48 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        fs: false,
-        path: require.resolve("path-browserify"),
-      };
-      return webpackConfig;
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
 };
