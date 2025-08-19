a3d344ee3ee9ea7565924a6b028af2a90318dad8
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-19 19:06:53 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import SharePage from "./components/SharePage";
 import ShareLinkPage from "./components/ShareLinkPage";
+import PersonalPage from "./components/PersonalPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* トップページ */}
         <Route path="/" element={<TopPage />} />
+
+        {/* 共有リンク作成ページ */}
         <Route path="/share" element={<SharePage />} />
-        <Route path="/share/:linkId" element={<ShareLinkPage />} />
-        <Route path="*" element={<TopPage />} />
+
+        {/* 個人ページ（必要なら） */}
+        <Route path="/personal" element={<PersonalPage />} />
+
+        {/* 共有リンク先ページ */}
+        <Route path="/link/:linkId" element={<ShareLinkPage />} />
       </Routes>
     </Router>
   );
