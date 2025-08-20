df330dcbfae97fdfc431332d66438599961bd674
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 17:39:49 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
-import LinkPage from "./components/LinkPage";
-import PersonalPage from "./components/PersonalPage";
-import SharePage from "./components/SharePage";
-import Header from "./components/Header";
 
 export default function App() {
   return (
     <Router>
-      {/* 共通バナー */}
-      <Header />
-
-      {/* ページルーティング */}
-      <div className="bg-black min-h-screen text-white">
-        <Routes>
-          <Route path="/" element={<TopPage />} />
-          <Route path="/link" element={<LinkPage />} />
-          <Route path="/personal" element={<PersonalPage />} />
-          <Route path="/share/:linkid" element={<SharePage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        {/* 他ページは今後追加 */}
+      </Routes>
     </Router>
   );
 }
