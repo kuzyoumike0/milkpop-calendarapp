71704135c0f48c856330c5d4a11f010feceac86c
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 13:59:40 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; // デザイン統一CSS
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
