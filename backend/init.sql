4558f84045e988c74c0b4a7c54a28edda8950fd7
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 14:09:24 +0900

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS schedules (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   memo TEXT,
-  dates TEXT[] NOT NULL,
+  dates DATE[] NOT NULL,
   timeslot TEXT NOT NULL,
   range_mode TEXT NOT NULL,
-  linkid TEXT
+  linkid TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS responses (
@@ -13,5 +13,5 @@ CREATE TABLE IF NOT EXISTS responses (
   linkid TEXT NOT NULL,
   username TEXT NOT NULL,
   answers JSONB NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP DEFAULT NOW()
 );
