90f6225b3c3ed0aab1f290af7650e9ffa95a0e93
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 12:57:03 +0900

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS schedules (
   id SERIAL PRIMARY KEY,
-  linkId TEXT NOT NULL,
+  linkid TEXT NOT NULL,
   title TEXT NOT NULL,
   date DATE NOT NULL,
-  timeslot TEXT NOT NULL,          -- 全日/昼/夜/時間指定
-  start_time TEXT,                 -- 時間指定開始
-  end_time TEXT,                   -- 時間指定終了
-  range_mode TEXT NOT NULL,        -- 範囲選択 or 複数選択
+  timeslot TEXT NOT NULL,
+  start_time TEXT,
+  end_time TEXT,
+  range_mode TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -14,6 +14,6 @@ CREATE TABLE IF NOT EXISTS responses (
   id SERIAL PRIMARY KEY,
   scheduleId INTEGER REFERENCES schedules(id) ON DELETE CASCADE,
   username TEXT NOT NULL,
-  response TEXT NOT NULL,          -- 〇 / ✖
+  response TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
