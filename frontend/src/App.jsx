3dec4695b924b860a3ef94fe0e70345b1f2768cc
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 14:33:54 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,24 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import LinkPage from "./components/LinkPage";
 import PersonalPage from "./components/PersonalPage";
-import SharePage from "./components/SharePage";
 import ShareLinkPage from "./components/ShareLinkPage";
+import SharePage from "./components/SharePage";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div className="bg-black min-h-screen text-white">
-        <header className="bg-[#FDB9C8] text-black p-4 flex justify-between items-center">
+        {/* バナー */}
+        <header className="bg-[#FDB9C8] text-black p-4 shadow-md flex justify-between items-center">
           <h1 className="text-2xl font-bold">MilkPOP Calendar</h1>
           <nav className="space-x-4">
-            <Link to="/" className="hover:underline">トップ</Link>
-            <Link to="/link" className="hover:underline">日程登録</Link>
-            <Link to="/personal" className="hover:underline">個人スケジュール</Link>
+            <Link to="/" className="hover:text-[#004CA0]">トップ</Link>
+            <Link to="/link" className="hover:text-[#004CA0]">日程登録</Link>
+            <Link to="/personal" className="hover:text-[#004CA0]">個人スケジュール</Link>
           </nav>
         </header>
+
+        {/* ページ遷移 */}
         <main className="p-6">
           <Routes>
             <Route path="/" element={<TopPage />} />
@@ -31,5 +34,3 @@ function App() {
     </Router>
   );
 }
-
-export default App;
