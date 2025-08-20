9034da73e8c8f77674936baaa545e17a644174a2
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 21:45:08 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import App from "./App";
+import TopPage from "./components/TopPage";
+import LinkPage from "./components/LinkPage";
+import PersonalPage from "./components/PersonalPage";
+import SharePage from "./components/SharePage";
+import ShareLinkPage from "./components/ShareLinkPage";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <App>
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/link" element={<LinkPage />} />
+        <Route path="/personal" element={<PersonalPage />} />
+        <Route path="/share/:linkid" element={<SharePage />} />
+        <Route path="/sharelink" element={<ShareLinkPage />} />
+      </Routes>
+    </App>
+  </Router>
 );
