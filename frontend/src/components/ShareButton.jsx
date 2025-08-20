1f1e98d14af25920484e9142f9b45daa12e4a87b
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 14:02:07 +0900

    Update ShareButton.jsx

--- frontend/src/components/ShareButton.jsx
+++ frontend/src/components/ShareButton.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function ShareButton({ link }) {
   return (
     <button
-      className="bg-[#FDB9C8] text-black px-4 py-2 rounded-xl"
-      onClick={() => navigator.clipboard.writeText(link)}
+      onClick={() => navigator.clipboard.writeText(window.location.origin + link)}
+      className="bg-[#FDB9C8] text-black px-4 py-2 rounded mt-2 shadow hover:scale-105"
     >
-      リンクコピー
+      リンクをコピー
     </button>
   );
 }
