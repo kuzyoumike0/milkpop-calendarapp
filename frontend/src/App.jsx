bacb8968263bfead1e685fe1fb2c404a621c30ad
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 18:14:35 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
-      こんにちは React!
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        こんにちは React + Tailwind!
+      </h1>
     </div>
   );
 }
