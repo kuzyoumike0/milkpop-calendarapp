480200b4cd2d487bb54b06c4a913f41345976582
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 18:13:34 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";   // App.jsx を読み込む
+import "./index.css";   // ← Tailwind 読み込み
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
