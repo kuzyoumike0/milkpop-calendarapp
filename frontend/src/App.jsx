ab5466bc65d0c76f8ffc4609a5644237ae0c3515
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 18:06:14 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TopPage from "./components/TopPage";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-      </Routes>
-    </Router>
+    <div>
+      こんにちは
+    </div>
   );
 }
