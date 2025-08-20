6507b596b7583eb920a0fb78ddae319b87de144d
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 19:51:38 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";   // ← Tailwind を適用するため必須
 import App from "./App";
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
