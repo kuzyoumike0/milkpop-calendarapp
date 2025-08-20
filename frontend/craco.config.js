62f392fa088ad8429a4dc7f38b312e83d5c0c670
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 17:37:36 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
+  style: {
+    postcss: {
+      plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+      ],
+    },
   },
-  plugins: [],
 };
