c8a288f7d0a025b2aa0d458d2f880de157a01547
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 06:39:44 +0900

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,10 +5,7 @@ import Header from "./Header";
 export default function Layout() {
   return (
     <div className="min-h-screen bg-black text-white font-sans relative">
-      {/* 共通ヘッダー */}
       <Header />
-
-      {/* 子ルートの内容をここに差し込む */}
       <div className="pt-20 px-4 relative z-10">
         <Outlet />
       </div>
