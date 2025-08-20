5204f6c3334f01d9807e03ed53f0926a13ddfe9d
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 20:40:36 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen relative z-10">
-      <h1 className="text-4xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-blue-500">
-        MilkPOP Calendar
-      </h1>
+      <h1 className="text-4xl font-bold">MilkPOP Calendar</h1>
     </div>
   );
 }
