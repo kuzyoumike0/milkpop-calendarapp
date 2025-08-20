7e7f4ca55536e511a7ee3ca634fc9aef0f5b795a
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 09:04:21 +0900

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -1,17 +1,21 @@
-CREATE TABLE IF NOT EXISTS schedules (
+DROP TABLE IF EXISTS responses CASCADE;
+DROP TABLE IF EXISTS schedules CASCADE;
+
+CREATE TABLE schedules (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   timeslot TEXT NOT NULL,
   range_mode TEXT NOT NULL,
-  linkid TEXT UNIQUE NOT NULL
+  linkid UUID NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS votes (
+CREATE TABLE responses (
   id SERIAL PRIMARY KEY,
-  linkid TEXT NOT NULL,
+  linkid UUID NOT NULL,
   username TEXT NOT NULL,
-  votes JSONB NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
+  schedule_id INT NOT NULL,
+  status TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
