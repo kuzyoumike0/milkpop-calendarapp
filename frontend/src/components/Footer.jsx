5712a885385ed87d2b6bdbd656c7cd17c6fa69e8
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 12:16:45 +0900

    Create Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#004CA0] text-center py-3 text-sm text-white">
+      © 2025 MilkPOP Calendar
+    </footer>
+  );
+}
