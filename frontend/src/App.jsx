64371e9336fd19753368b708def3e7f7d7143d4d
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 18:10:59 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function App() {
-  return <h1 style={{ color: "red" }}>こんにちは React!</h1>;
+  return (
+    <div>
+      こんにちは React!
+    </div>
+  );
 }
