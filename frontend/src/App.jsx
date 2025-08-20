26517b3c89d60fa9e11c91a945ff242ac68654fe
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 15:18:05 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import LinkPage from "./components/LinkPage";
 import PersonalPage from "./components/PersonalPage";
 import SharePage from "./components/SharePage";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-black text-white">
-        {/* Banner */}
-        <header className="bg-[#004CA0] text-white p-4 shadow-lg flex justify-between items-center">
-          <h1 className="text-2xl font-bold">MilkPOP Calendar</h1>
-          <nav className="space-x-4">
-            <Link to="/" className="hover:text-[#FDB9C8]">トップ</Link>
-            <Link to="/link" className="hover:text-[#FDB9C8]">日程登録</Link>
-            <Link to="/personal" className="hover:text-[#FDB9C8]">個人日程</Link>
-          </nav>
-        </header>
+      {/* 共通バナー */}
+      <Header />
 
-        <main className="p-6">
-          <Routes>
-            <Route path="/" element={<TopPage />} />
-            <Route path="/link" element={<LinkPage />} />
-            <Route path="/personal" element={<PersonalPage />} />
-            <Route path="/share/:linkid" element={<SharePage />} />
-          </Routes>
-        </main>
+      {/* ページルーティング */}
+      <div className="bg-black min-h-screen text-white">
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/link" element={<LinkPage />} />
+          <Route path="/personal" element={<PersonalPage />} />
+          <Route path="/share/:linkid" element={<SharePage />} />
+        </Routes>
       </div>
     </Router>
   );
