a5b4112009c86cfc7c3236579301a50d8d09206d
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 20:33:35 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-4xl font-bold text-red-500">
-        Tailwind TEST
+    <div className="flex items-center justify-center min-h-screen relative z-10">
+      <h1 className="text-4xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-blue-500">
+        MilkPOP Calendar
       </h1>
     </div>
   );
