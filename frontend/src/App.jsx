4b69b723ee5cd359135197f84fca9b30ed4319a5
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 06:25:33 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import TopPage from "./components/TopPage";
 import LinkPage from "./components/LinkPage";
 import PersonalPage from "./components/PersonalPage";
@@ -9,22 +10,24 @@ import ShareLinkPage from "./components/ShareLinkPage";
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* トップページ */}
-        <Route path="/" element={<TopPage />} />
+      <Layout>
+        <Routes>
+          {/* トップページ */}
+          <Route path="/" element={<TopPage />} />
 
-        {/* 日程登録ページ */}
-        <Route path="/link" element={<LinkPage />} />
+          {/* 日程登録ページ */}
+          <Route path="/link" element={<LinkPage />} />
 
-        {/* 個人日程登録ページ */}
-        <Route path="/personal" element={<PersonalPage />} />
+          {/* 個人日程登録ページ */}
+          <Route path="/personal" element={<PersonalPage />} />
 
-        {/* 共有ページ（登録したスケジュールの回答用） */}
-        <Route path="/share/:linkid" element={<SharePage />} />
+          {/* 共有ページ（登録したスケジュールの回答用） */}
+          <Route path="/share/:linkid" element={<SharePage />} />
 
-        {/* 共有リンクページ（発行されたリンクを表示） */}
-        <Route path="/sharelink/:linkid" element={<ShareLinkPage />} />
-      </Routes>
+          {/* 共有リンクページ（発行されたリンクを表示） */}
+          <Route path="/sharelink/:linkid" element={<ShareLinkPage />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
