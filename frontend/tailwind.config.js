c4d0992b0b5ddc5718b77b29db7ad05b7cd85cc3
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 16:34:40 +0900

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// frontend/tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        brandPink: "#FDB9C8",
+        brandBlue: "#004CA0",
+        brandBlack: "#0A0A0A",
+      },
+    },
+  },
+  plugins: [],
+};
