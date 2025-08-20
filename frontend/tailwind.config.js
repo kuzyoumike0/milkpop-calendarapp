2ff786f05c4307d865f7939777c5468832088e6b
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-20 21:47:53 +0900

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        gradientShift: {
-          "0%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
-          "100%": { backgroundPosition: "0% 50%" },
-        },
-      },
-      animation: {
-        gradientShift: "gradientShift 20s ease infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
