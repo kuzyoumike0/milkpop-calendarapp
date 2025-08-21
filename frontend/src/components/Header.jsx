679d9ba8a05d84ee65aaff66776f5ac7657a103c
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 05:30:37 +0900

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+// frontend/src/components/Header.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
@@ -5,7 +6,7 @@ import "../index.css";
 const Header = () => {
   return (
     <header className="banner">
-      <h1 className="logo">MilkPOP Calendar</h1>
+      <h1>MilkPOP Calendar</h1>
       <nav>
         <Link to="/">トップ</Link>
         <Link to="/share">日程登録</Link>
