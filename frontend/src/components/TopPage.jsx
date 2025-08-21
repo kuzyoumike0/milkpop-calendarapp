fbd1a85a3203131ab0f0f62fb56e1b87e5ea11b3
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 06:49:43 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,13 +1,13 @@
 // frontend/src/components/TopPage.jsx
 import React from "react";
-import logo from "../public/logo.png";
 import "../index.css";
 
 const TopPage = () => {
   return (
     <div className="top-page">
+      {/* グラスモーフィズム背景を敷いたロゴ */}
       <div className="logo-wrapper">
-        <img src={logo} alt="logo" className="logo-image" />
+        <img src="/logo.png" alt="MilkPOP Logo" className="logo-image" />
       </div>
       <h2 className="welcome-text">ようこそ MilkPOP Calendar へ</h2>
     </div>
