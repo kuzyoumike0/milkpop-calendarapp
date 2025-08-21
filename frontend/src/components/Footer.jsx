c9f3509f2f960579fe1877b1a2f814f95c48f8e7
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 05:36:25 +0900

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import "../index.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2025 MilkPOP Calendar. All rights reserved.</p>
+      © 2025 MilkPOP Calendar. All rights reserved.
     </footer>
   );
 };
