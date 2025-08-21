1242e1165734260de8f4626fff00dcfba0c03396
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 18:31:30 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,7 @@
-// frontend/src/App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TopPage from "./components/TopPage";
-import LinkPage from "./components/LinkPage";
-import PersonalPage from "./components/PersonalPage";
-import SharePage from "./components/SharePage";
-
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white">
-      <Router>
-        <Routes>
-          <Route path="/" element={<TopPage />} />
-          <Route path="/link" element={<LinkPage />} />
-          <Route path="/personal" element={<PersonalPage />} />
-          <Route path="/share/:linkid" element={<SharePage />} />
-        </Routes>
-      </Router>
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl text-pink">Tailwind テスト</h1>
     </div>
   );
 }
