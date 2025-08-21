8613c99d20bc3d0461b125d839754a6b333c6076
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 13:39:08 +0900

    Create tailwind.config.js

---
+++ frontend/public/tailwind.config.js
@@ -0,0 +1,11 @@
+// frontend/tailwind.config.js
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
