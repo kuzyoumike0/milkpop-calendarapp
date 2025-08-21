f89812e2facc3927dacdc6f2e89a5ce1c38543ee
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 12:55:46 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import TopPage from "./components/TopPage";
 import LinkPage from "./components/LinkPage";
@@ -9,17 +9,15 @@ import ShareLinkPage from "./components/ShareLinkPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<TopPage />} />
-          <Route path="link" element={<LinkPage />} />
-          <Route path="personal" element={<PersonalPage />} />
-          <Route path="share/:linkid" element={<SharePage />} />
-          <Route path="sharelink/:linkid" element={<ShareLinkPage />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<TopPage />} />
+        <Route path="link" element={<LinkPage />} />
+        <Route path="personal" element={<PersonalPage />} />
+        <Route path="share/:linkid" element={<SharePage />} />
+        <Route path="sharelink/:linkid" element={<ShareLinkPage />} />
+      </Route>
+    </Routes>
   );
 }
 
