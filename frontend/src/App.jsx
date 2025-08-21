7b174c44a15fb8ff3b290ed995a6799c7eb4b182
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 17:52:43 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,26 @@ import TopPage from "./components/TopPage";
 import LinkPage from "./components/LinkPage";
 import PersonalPage from "./components/PersonalPage";
 import SharePage from "./components/SharePage";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-        <Route path="/link" element={<LinkPage />} />
-        <Route path="/personal" element={<PersonalPage />} />
-        <Route path="/share/:linkid" element={<SharePage />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen bg-black text-white">
+        <Banner />
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/link" element={<LinkPage />} />
+            <Route path="/personal" element={<PersonalPage />} />
+            <Route path="/share/:linkid" element={<SharePage />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </Router>
   );
 }
