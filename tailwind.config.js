e3215f1390877b811ecd2f8ed6b3ecdb775211ed
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 22:11:49 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React のソースを監視
+    "./public/index.html"
+  ],
   theme: {
     extend: {
       colors: {
-        pinkCustom: "#FDB9C8",
+        primary: "#004CA0",
+        accent: "#FDB9C8"
       },
-    },
+      fontFamily: {
+        sans: ["Soleil", "sans-serif"] // ローカルフォント設定
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
