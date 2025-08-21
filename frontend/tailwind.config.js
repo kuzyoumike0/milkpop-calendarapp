3abd31381f41cb960be39df446759ab6465f2457
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 18:07:46 +0900

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./public/index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",   // ✅ src 以下の全てのファイルを監視
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         pink: "#FDB9C8",
         deepblue: "#004CA0",
-        luxuryblack: "#000000",
-      },
-      fontFamily: {
-        sans: ["Poppins", "Noto Sans JP", "sans-serif"],
       },
     },
   },
