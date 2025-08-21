8b24fa081e98a7751611d54e5a86531152f3c500
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 00:31:59 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "react-big-calendar/lib/css/react-big-calendar.css"; // カレンダーCSS
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import App from "./App";
 
-// Chakra UI を使う場合
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: "black",
-        color: "white",
+        bg: "transparent", // ← 黒から透明に変更！
+        color: "#333",     // ← CSSの文字色と統一
       },
     },
   },
