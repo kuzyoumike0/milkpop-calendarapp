c7b48fe8affe869768ae70e08ca4da8ba35eebc9
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 16:21:44 +0900

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,18 @@
 // frontend/tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
+  safelist: [
+    "bg-gray-900",
+    "hover:bg-gray-800",
+    "text-white",
+    "rounded-2xl",
+    "shadow-lg",
+    "transition",
+    "transform",
+    "hover:scale-105",
+  ],
   plugins: [],
 };
