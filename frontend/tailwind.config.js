cdee05385bf57cd7a59c41eb6e531c10cc1b60ee
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 16:08:31 +0900

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,5 @@
-// frontend/tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Reactのsrc以下をスキャン
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
