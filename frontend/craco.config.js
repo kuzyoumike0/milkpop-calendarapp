655f4b80059eb7817956f4fe82582bdeeab0dd1e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 16:56:34 +0900

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,10 +3,7 @@ const path = require("path");
 module.exports = {
   style: {
     postcss: {
-      plugins: [
-        require("tailwindcss"),
-        require("autoprefixer"),
-      ],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
   webpack: {
