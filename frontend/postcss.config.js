4c165cdffeedb67df9aaa3990ad84dc38753a0da
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 18:16:06 +0900

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,14 +1,6 @@
-// frontend/src/components/TopPage.jsx
-import React from "react";
-
-function TopPage() {
-  return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-pink">
-        トップページテスト
-      </h1>
-    </div>
-  );
-}
-
-export default TopPage;
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
