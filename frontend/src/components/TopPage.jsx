2d54d8e7ab6d87ca7e79bbbd7cf684cab772e4b8
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 05:52:15 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -4,9 +4,7 @@ import "../index.css";
 const TopPage = () => {
   return (
     <div className="top-page">
-      <h2>ようこそ MilkPOP Calendar へ</h2>
       <img src="/logo.png" alt="トップ画像" className="top-image" />
-      <p>ここから日程を登録したり、個人スケジュールを追加できます。</p>
     </div>
   );
 };
