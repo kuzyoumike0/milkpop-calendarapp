6f8b92f2cdc52e6c7888724dd6ff3aaba306485e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 13:16:32 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
+// frontend/src/App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import TopPage from "./components/TopPage";
+import LinkPage from "./components/LinkPage";
+import PersonalPage from "./components/PersonalPage";
+import SharePage from "./components/SharePage";
+import ShareLinkPage from "./components/ShareLinkPage";
+
 function App() {
   return (
-    <div className="bg-red-500 text-white text-4xl font-bold p-10">
-      Tailwind Test
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<TopPage />} />
+        <Route path="link" element={<LinkPage />} />
+        <Route path="personal" element={<PersonalPage />} />
+        <Route path="share/:linkid" element={<SharePage />} />
+        <Route path="sharelink/:linkid" element={<ShareLinkPage />} />
+      </Route>
+    </Routes>
   );
 }
+
+export default App;
