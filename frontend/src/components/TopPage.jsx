9628fac15fadb9bea8f7e177751cd5b390bbb577
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 01:03:34 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -24,7 +24,7 @@ const TopPage = () => {
             src="/logo.png"   // ← public 配下は / から参照
             alt="ロゴ"
             style={{
-              width: "6400px",   // 小さめ
+              width: "840px",   // 小さめ
               height: "auto",
               margin: "20px auto",
               display: "block",
