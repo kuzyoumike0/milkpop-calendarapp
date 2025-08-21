5a4da9d1e0c7dd5bc0a734d07ee02003a8f2289a
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 23:20:17 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// frontend/src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TopPage from "./components/TopPage";
@@ -5,6 +6,7 @@ import SharePage from "./components/SharePage";
 import PersonalPage from "./components/PersonalPage";
 import ShareLinkPage from "./components/ShareLinkPage";
 import LinkPage from "./components/LinkPage";
+import RegisterPage from "./components/RegisterPage";  // ← 追加
 
 function App() {
   return (
@@ -13,25 +15,20 @@ function App() {
       <div className="bg-[#004CA0] text-white p-4 flex justify-between items-center">
         <h1 className="text-xl font-bold">MilkPOP Calendar</h1>
         <nav className="flex gap-4">
-          <Link to="/" className="hover:text-[#FDB9C8]">
-            トップ
-          </Link>
-          <Link to="/link" className="hover:text-[#FDB9C8]">
-            日程登録
-          </Link>
-          <Link to="/personal" className="hover:text-[#FDB9C8]">
-            個人スケジュール
-          </Link>
+          <Link to="/" className="hover:text-[#FDB9C8]">トップ</Link>
+          <Link to="/register" className="hover:text-[#FDB9C8]">日程登録</Link> {/* ← 修正 */}
+          <Link to="/personal" className="hover:text-[#FDB9C8]">個人スケジュール</Link>
         </nav>
       </div>
 
       {/* ページルーティング */}
       <Routes>
         <Route path="/" element={<TopPage />} />
-        <Route path="/share/:id" element={<SharePage />} />   {/* ← 共有リンクページ */}
+        <Route path="/share/:id" element={<SharePage />} />
         <Route path="/personal" element={<PersonalPage />} />
         <Route path="/sharelink" element={<ShareLinkPage />} />
-        <Route path="/link" element={<LinkPage />} />          {/* ← 日程登録ページ */}
+        <Route path="/link" element={<LinkPage />} />
+        <Route path="/register" element={<RegisterPage />} /> {/* ← 追加 */}
       </Routes>
     </Router>
   );
