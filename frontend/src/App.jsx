b8ae838c92135d3fcbb308cef017ac0c5a8246a5
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 16:12:15 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,16 @@
-// frontend/src/App.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import TopPage from "./components/TopPage";
-import LinkPage from "./components/LinkPage";
-import PersonalPage from "./components/PersonalPage";
-import SharePage from "./components/SharePage";
-import ShareLinkPage from "./components/ShareLinkPage";
+import LinksSection from "./components/LinksSection";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<TopPage />} />
-        <Route path="link" element={<LinkPage />} />
-        <Route path="personal" element={<PersonalPage />} />
-        <Route path="share/:linkid" element={<SharePage />} />
-        <Route path="sharelink/:linkid" element={<ShareLinkPage />} />
-      </Route>
-    </Routes>
+    <div className="min-h-screen bg-black text-white">
+      <header className="py-6 text-center text-3xl font-extrabold text-pink-300">
+        🌸 Calendar App
+      </header>
+      <main>
+        <LinksSection />
+      </main>
+    </div>
   );
 }
 
