d036bd2c085dfbdd51315e49d64da7e508a08f3f
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 18:42:39 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,14 +1,7 @@
-// frontend/src/components/TopPage.jsx
-import React from "react";
-
 function TopPage() {
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-pink">
-        トップページテスト
-      </h1>
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl">Tailwind Test - 背景黒？</h1>
     </div>
   );
 }
-
-export default TopPage;
