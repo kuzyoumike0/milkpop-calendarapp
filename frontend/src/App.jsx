a441503de7f23d9e5e1dfe5eb10eafa0e96e287c
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 05:32:24 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,25 @@
+// frontend/src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
-import SharePage from "./components/SharePage";
+import RegisterPage from "./components/RegisterPage";
 import PersonalPage from "./components/PersonalPage";
-import ShareLinkPage from "./components/ShareLinkPage";
-import LinkPage from "./components/LinkPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./index.css";
 
 function App() {
   return (
     <Router>
       <div className="page-container">
-        <Header />
+        <Header /> {/* ヘッダーを表示 */}
 
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<TopPage />} />
-            <Route path="/share" element={<SharePage />} />
-            <Route path="/personal" element={<PersonalPage />} />
-            <Route path="/sharelink" element={<ShareLinkPage />} />
-            <Route path="/link" element={<LinkPage />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/share" element={<RegisterPage />} />
+          <Route path="/personal" element={<PersonalPage />} />
+        </Routes>
 
-        <Footer />
+        <Footer /> {/* フッターを表示 */}
       </div>
     </Router>
   );
