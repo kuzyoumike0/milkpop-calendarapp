9868d2c0398b48ce44dc4ffa5d469188b76c38a0
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-21 13:10:11 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,24 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-function TopPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-10 bg-black">
-      <Link
-        to="/link"
-        className="block w-80 p-6 bg-pink-500 text-white rounded-xl shadow-lg text-center hover:bg-pink-600 transition"
-      >
-        日程登録ページ
-      </Link>
-
-      <Link
-        to="/personal"
-        className="block w-80 p-6 bg-blue-600 text-white rounded-xl shadow-lg text-center hover:bg-blue-700 transition"
-      >
-        個人スケジュール
-      </Link>
-    </div>
-  );
-}
-
-export default TopPage;
+<Link
+  to="/link"
+  className="block w-80 p-6 bg-pink-500 text-white rounded-xl shadow-lg text-center hover:bg-pink-600 transition"
+>
+  日程登録ページ
+</Link>
