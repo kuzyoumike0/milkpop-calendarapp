771c53d2f42e351d016990432f192219a5320921
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 12:19:41 +0900

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,20 +4,14 @@ import "../index.css";
 
 const Header = () => {
   return (
-    <>
-      <header className="banner">
-        <h1>MilkPOP Calendar</h1>
-        <nav>
-          <Link to="/">トップ</Link>
-          <Link to="/share">日程登録</Link>
-          <Link to="/personal">個人スケジュール</Link>
-        </nav>
-      </header>
-      {/* ヘッダー下に画像 */}
-      <div className="header-image">
-        <img src="/header-bg.png" alt="Header Background" />
-      </div>
-    </>
+    <header className="banner">
+      <h1>MilkPOP Calendar</h1>
+      <nav>
+        <Link to="/">トップ</Link>
+        <Link to="/share">日程登録</Link>
+        <Link to="/personal">個人スケジュール</Link>
+      </nav>
+    </header>
   );
 };
 
