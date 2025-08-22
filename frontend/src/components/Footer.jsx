fb44cd98f6d93b0cd71ec76024b3306c5ad07ee9
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 12:12:32 +0900

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,9 +7,6 @@ const Footer = () => {
       <footer className="footer">
         <p>© 2025 MilkPOP Calendar. All rights reserved.</p>
       </footer>
-      {/* フッター下に画像 */}
-      <div className="footer-image">
-        <img src="/footer-bg.png" alt="Footer Background" />
       </div>
     </>
   );
