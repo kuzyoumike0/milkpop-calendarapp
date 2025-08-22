5193ae12f5d89b07924081fb29a0a801e9a26425
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 21:18:16 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import "./index.css";   // 自作CSSを読み込み
+import "./index.css";   // ✅ これを一番最後に書く
 import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
