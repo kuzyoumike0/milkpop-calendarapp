41546ce728b3a91dc3c75330dcc1a7d689589fdd
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 12:20:01 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -4,8 +4,14 @@ import "../index.css";
 const TopPage = () => {
   return (
     <div className="page-container">
+      {/* トップ画像 */}
+      <div className="top-image">
+        <img src="/header-bg.png" alt="Top Visual" />
+      </div>
+
       <div className="card" style={{ textAlign: "center" }}>
         <img
+          src="/logo.png"
           alt="MilkPOP Logo"
           style={{ width: "180px", marginBottom: "20px" }}
         />
