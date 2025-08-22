b35ff4c1116a51a5367a32bb62eb60e3069e80f4
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 00:42:33 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,44 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Flex, Heading, Spacer, HStack, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import Header from "./Header";
+import Footer from "./Footer";
+import logo from "../assets/logo.png"; // public/logo.png なら "/logo.png" でOK
 
-const Header = () => {
+const TopPage = () => {
   return (
-    <Flex
-      as="header"
-      bgGradient="linear(to-r, #FDB9C8, #004CA0)"
-      color="white"
-      p={4}
-      align="center"
-      position="fixed"
-      top="0"
-      left="0"
-      w="100%"
-      zIndex="1000"
-    >
-      <Heading size="md" letterSpacing="1px">
-        MilkPOP Calendar
-      </Heading>
-      <Spacer />
-      <HStack spacing={8}>
-        <Link to="/register">
-          <Text fontWeight="bold" _hover={{ color: "#ff5fa2" }}>
-            日程登録
-          </Text>
-        </Link>
-        <Link to="/personal">
-          <Text fontWeight="bold" _hover={{ color: "#ff5fa2" }}>
-            個人スケジュール
-          </Text>
-        </Link>
-        <Link to="/share-links">
-          <Text fontWeight="bold" _hover={{ color: "#ff5fa2" }}>
-            共有リンク一覧
-          </Text>
-        </Link>
-      </HStack>
+    <Flex direction="column" minH="100vh" bg="brand.lightPink">
+      {/* ===== ヘッダー ===== */}
+      <Header />
+
+      {/* ===== メイン ===== */}
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        justify="center"
+        flex="1"
+        pt="120px"
+        pb="60px"
+      >
+        <Box mb={10}>
+          <Image src={logo} alt="MilkPOP Logo" maxW="400px" />
+        </Box>
+
+        <Heading as="h2" size="lg" color="brand.blue" mb={6}>
+          ようこそ、MilkPOP Calendar へ
+        </Heading>
+        <Text color="gray.700" textAlign="center" maxW="lg">
+          仲間と予定を共有したり、自分だけのスケジュールを管理できます。
+          <br />
+          上のメニューから始めましょう！
+        </Text>
+      </Flex>
+
+      {/* ===== フッター ===== */}
+      <Footer />
     </Flex>
   );
 };
 
-export default Header;
+export default TopPage;
