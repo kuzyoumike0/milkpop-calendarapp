1b771130b3ef1b8ae96014fe53d741e281e8514b
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 19:08:50 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,33 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import TopPage from "./components/TopPage";
 import RegisterPage from "./components/RegisterPage";
 import PersonalPage from "./components/PersonalPage";
 import SharePage from "./components/SharePage";
+import ShareLinkPage from "./components/ShareLinkPage";
+import LinkPage from "./components/LinkPage";
+import Footer from "./components/Footer";
+import "./index.css";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/personal" element={<PersonalPage />} />
-        <Route path="/share/:id" element={<SharePage />} />
-      </Routes>
+      {/* ✅ 全体を縦方向flexにしてフッター固定 */}
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/personal" element={<PersonalPage />} />
+            <Route path="/share" element={<SharePage />} />
+            <Route path="/sharelink" element={<ShareLinkPage />} />
+            <Route path="/link" element={<LinkPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
