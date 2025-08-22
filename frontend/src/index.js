e0c9bd24b385c8de89a4d7d41856a6364868e037
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 00:26:34 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import "./index.css";   // ✅ これを一番最後に書く
 import App from "./App";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
