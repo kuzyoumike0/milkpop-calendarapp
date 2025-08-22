d613d502b04f958f36b0e280e4da349deae8aba1
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 09:50:54 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// frontend/src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
@@ -5,27 +6,17 @@ import RegisterPage from "./components/RegisterPage";
 import PersonalPage from "./components/PersonalPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./index.css";
 
 function App() {
   return (
     <Router>
-      <div className="app-container flex flex-col min-h-screen">
-        {/* ヘッダー */}
-        <Header />
-
-        {/* ページ本体 */}
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<TopPage />} />
-            <Route path="/share" element={<RegisterPage />} />
-            <Route path="/personal" element={<PersonalPage />} />
-          </Routes>
-        </main>
-
-        {/* フッター */}
-        <Footer />
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/personal" element={<PersonalPage />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
