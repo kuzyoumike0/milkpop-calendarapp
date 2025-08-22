fde74163cb3de6f0adf023742a1acf3190acf8a9
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 21:15:15 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 // frontend/src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";   // ✅ Tailwindと全体のCSSを読み込む
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";   // 自作CSSを読み込み
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
