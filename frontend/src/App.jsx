1a345ad35877eb1dea000d0768bca1f36de87c3b
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 18:34:37 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,20 @@
-// frontend/src/App.jsx
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import TopPage from "./components/TopPage";
 import RegisterPage from "./components/RegisterPage";
 import PersonalPage from "./components/PersonalPage";
 import SharePage from "./components/SharePage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import "./index.css";
 
 function App() {
   return (
     <Router>
       <Header />
-      <main className="main-container">
-        <Routes>
-          <Route path="/" element={<TopPage />} /> {/* ✅ トップページ */}
-          <Route path="/register" element={<RegisterPage />} /> {/* ✅ 日程登録ページ */}
-          <Route path="/personal" element={<PersonalPage />} />
-          <Route path="/share/:id" element={<SharePage />} />
-        </Routes>
-      </main>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/personal" element={<PersonalPage />} />
+        <Route path="/share/:id" element={<SharePage />} />
+      </Routes>
     </Router>
   );
 }
