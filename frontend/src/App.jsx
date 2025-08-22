bb651211bf85feef95630f77ee415469107a7e57
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 16:07:34 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
-import RegisterPage from "./components/RegisterPage"; 
+import RegisterPage from "./components/RegisterPage";
 import PersonalPage from "./components/PersonalPage";
+import SharePage from "./components/SharePage";  // 👈 追加
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./index.css";
@@ -15,11 +16,14 @@ function App() {
 
         <main className="main-content">
           <Routes>
-            {/* ルートにトップページを割り当て */}
+            {/* トップページ */}
             <Route path="/" element={<TopPage />} />
 
             {/* 日程登録ページ */}
             <Route path="/share" element={<RegisterPage />} />
+
+            {/* 共有リンクページ（idごとに表示） */}
+            <Route path="/share/:id" element={<SharePage />} />   {/* 👈 新規追加 */}
 
             {/* 個人スケジュールページ */}
             <Route path="/personal" element={<PersonalPage />} />
