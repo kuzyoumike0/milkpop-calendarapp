28c40c172bccfdbc3e2192618cb18225397064ca
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 00:27:58 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 );
