8c474da294794fd8484dc0ba5b810a668f27bfd3
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-22 21:03:04 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,12 @@
 // frontend/src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// react-calendar のデフォルトCSSを先に読む
-import "react-calendar/dist/Calendar.css";
-
-// あとから自作CSSを上書きする
-import "./index.css";
-
+import "./index.css";   // ✅ Tailwind & 全体CSS
 import App from "./App";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-const theme = extendTheme({});
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
-  <ChakraProvider theme={theme}>
+  <React.StrictMode>
     <App />
-  </ChakraProvider>
+  </React.StrictMode>
 );
