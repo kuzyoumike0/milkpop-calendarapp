828cf68d8fb23d445aa1d2a8842f4e65913727d4
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 06:49:44 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,17 @@
+// frontend/src/App.js
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import TopPage from "./components/TopPage";
 import RegisterPage from "./components/RegisterPage";
-import TopPage from "./pages/TopPage";
-import SharePage from "./pages/SharePage";
+import SharePage from "./components/SharePage";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<TopPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/share/:id" element={<SharePage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<TopPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/share/:id" element={<SharePage />} />
+    </Routes>
   );
 }
 
