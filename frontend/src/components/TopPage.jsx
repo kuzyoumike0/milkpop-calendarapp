002e1e6eba44a788508dadf2d1bca9103d756726
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 17:18:14 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import "./index.css";
+
 
 const TopPage = () => {
   return (
