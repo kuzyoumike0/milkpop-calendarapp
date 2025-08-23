b3d4eb1b282015ed44aeae46c6e76d4571a7a8d0
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 16:51:13 +0900

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white text-center py-3 mt-auto">
-      <p className="text-sm">© 2025 MilkPOP Calendar</p>
+    <footer className="main-footer">
+      <p>© 2025 MilkPOP Calendar - All Rights Reserved</p>
     </footer>
   );
 };
