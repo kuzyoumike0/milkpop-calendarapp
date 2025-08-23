6d295f81d275955484704200b367f9b3059ef7e4
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 20:45:14 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,17 @@
 // frontend/src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// ここで必ず react-calendar を先に読み込む
-import "react-calendar/dist/Calendar.css";
-
-// その後に自作CSSを読み込む
+// ✅ グローバルCSSをここで読み込む
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
