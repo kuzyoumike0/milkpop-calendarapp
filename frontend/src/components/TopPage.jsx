a2d2eed0c006014adb72672caed2d1fedf0a162c
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 14:55:30 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -11,7 +10,9 @@ const TopPage = () => {
       <main className="flex-grow flex flex-col items-center px-6 pb-20">
         <div className="max-w-4xl w-full bg-white rounded-2xl shadow-xl p-12 text-center space-y-10 mt-20">
           {/* 見出し */}
-          <h2 className="text-3xl font-bold text-[#004CA0]">予定管理スケジュールができます</h2>
+          <h2 className="text-3xl font-bold text-[#004CA0]">
+            予定管理が簡単にできます！
+          </h2>
 
           {/* ロゴ画像 */}
           <div className="flex justify-center">
@@ -20,6 +21,12 @@ const TopPage = () => {
               alt="MilkPOP Calendar ロゴ"
               className="top-logo rounded-2xl shadow-2xl"
             />
+          </div>
+
+          {/* メッセージ */}
+          <p className="text-lg text-gray-700 text-center">
+            ここから日程登録ページや個人スケジュールページに移動できます。
+          </p>
         </div>
       </main>
 
