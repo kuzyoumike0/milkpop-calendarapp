983477531e129b34b053785b4cbf3f36b52ee673
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 07:26:33 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,34 @@
-// frontend/src/App.js
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./components/TopPage";
 import RegisterPage from "./components/RegisterPage";
+import PersonalPage from "./components/PersonalPage";
 import SharePage from "./components/SharePage";
+import ShareLinkPage from "./components/ShareLinkPage";
+import LinkPage from "./components/LinkPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-      <main>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        {/* ✅ 全ページ共通のヘッダー */}
+        <Header />
+
         <Routes>
           <Route path="/" element={<TopPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/share/:id" element={<SharePage />} />
+          <Route path="/personal" element={<PersonalPage />} />
+          <Route path="/share" element={<SharePage />} />
+          <Route path="/share/:id" element={<ShareLinkPage />} />
+          <Route path="/links" element={<LinkPage />} />
         </Routes>
-      </main>
-      <Footer />
-    </div>
+
+        {/* ✅ 全ページ共通のフッター */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
