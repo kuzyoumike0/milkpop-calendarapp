b370640b3630230a530f2d6b1f90edc3fa0d239a
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 06:41:33 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import RegisterPage from "./pages/RegisterPage";
+import RegisterPage from "./RegisterPage";
 import TopPage from "./pages/TopPage";
 import SharePage from "./pages/SharePage";
 
