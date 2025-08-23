706fc1261b6fd0744a42fa432e78ec1bcd7a2d50
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 07:30:12 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-// ✅ グローバルCSSだけ読み込む
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
