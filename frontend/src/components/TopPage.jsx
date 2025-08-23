f7a66c1c730d78aec5e4903d46a970ce12433f92
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-23 17:20:09 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -8,6 +8,13 @@ const TopPage = () => {
     <>
       <Header />
 
+
+              <div className="card nav-card">
+          <h2 className="title">はじめよう！</h2>
+          <p className="subtitle">
+            予定を登録して、みんなと共有しましょう。
+          </p>
+        </div>
       <main className="top-page">
         <img
           src="/logo.png"
@@ -15,18 +22,7 @@ const TopPage = () => {
           className="top-logo"
         />
 
-        <div className="card nav-card">
-          <h2 className="title">はじめよう！</h2>
-          <p className="subtitle">
-            予定を登録して、みんなと共有しましょう。
-          </p>
 
-          <div className="nav-links">
-            <Link to="/register" className="save-btn">日程登録ページへ</Link>
-            <Link to="/personal" className="save-btn">個人スケジュールへ</Link>
-            <Link to="/share" className="save-btn">共有ページへ</Link>
-          </div>
-        </div>
       </main>
 
       <Footer />
