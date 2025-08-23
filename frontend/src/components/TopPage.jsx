21e2bb02cb5806a39f56351b4c7599997c370c4e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 07:15:22 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -2,16 +2,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../index.css";
+import logo from "../public/logo.png"; // public に置いた logo.png を読み込み
 
 const TopPage = () => {
   return (
-    <main className="flex-grow container mx-auto px-4 py-12 text-center">
-      <h1 className="text-4xl font-bold mb-6 text-[#004CA0]">
-        MilkPOP Calendar
-      </h1>
+    
       <p className="text-lg mb-8 text-gray-700">
         あなたの予定を簡単に登録・共有できるカレンダーサービスです。
       </p>
+    <main className="flex-grow container mx-auto px-4 py-12 text-center">
+      {/* ロゴ */}
+      <div className="flex justify-center mb-10">
+        <img
+          src={logo}
+          alt="MilkPOP Calendar Logo"
+          className="max-w-full"
+          style={{ width: "1040px" }}
+        />
+      </div>
 
 
     </main>
