037414214fa367ba6564df939607cde95b708fe5
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 05:01:52 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// ✅ グローバルCSSをここで読み込む
+// ✅ グローバルCSSだけ読み込む
 import "./index.css";
-import "react-calendar/dist/Calendar.css"; // ← 追加
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
