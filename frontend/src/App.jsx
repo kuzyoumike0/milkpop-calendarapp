ed152a3861a232b7fbdb3bbe0792dff59212901e
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 10:40:35 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import TopPage from "./components/TopPage";
 import PersonalPage from "./components/PersonalPage";
 import RegisterPage from "./components/RegisterPage";
+import SharePage from "./components/SharePage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<TopPage />} />
           <Route path="/personal" element={<PersonalPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/share/:id" element={<SharePage />} /> {/* 追加 */}
         </Routes>
       </main>
       <Footer />
