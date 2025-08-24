92c7bb493f24a140ac68a21bdee79079fe439d53
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 09:25:55 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+// frontend/src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -9,11 +10,13 @@ function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-        <Route path="/personal" element={<PersonalPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
+      <main style={{ flex: "1" }}>
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/personal" element={<PersonalPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </main>
       <Footer />
     </Router>
   );
