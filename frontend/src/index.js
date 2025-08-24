ae44dfebb6c76605faf4a2f06671cefd38eb61f5
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 09:48:37 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,13 @@
-// frontend/src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import TopPage from "./components/TopPage";
-import PersonalPage from "./components/PersonalPage";
-import RegisterPage from "./components/RegisterPage";
+// frontend/src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <main style={{ flex: "1" }}>
-        <Routes>
-          <Route path="/" element={<TopPage />} />
-          <Route path="/personal" element={<PersonalPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
-  );
-}
-
-export default App;
+// React 18 用のレンダリング
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
