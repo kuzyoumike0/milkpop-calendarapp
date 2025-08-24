25801c020842c96927ae118eb645111d6f3f7ec4
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-24 09:31:36 +0900

    Update TopPage.jsx

--- frontend/src/components/TopPage.jsx
+++ frontend/src/components/TopPage.jsx
@@ -7,7 +7,7 @@ const TopPage = () => {
       <div className="card">
         <h1 className="page-title">ようこそ！MilkPOP Calendar</h1>
         <p className="page-text">
-          予定を登録して、友達や仲間と簡単に共有できるカレンダーです。<br />
+          予定を登録して友達や仲間と簡単に共有できるカレンダーです。<br />
           上のメニューから「日程登録」や「個人スケジュール」を使ってください。
         </p>
       </div>
