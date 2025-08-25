c4b42984c7fbb528e53b07bd40102269bf630f70
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-25 10:48:07 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TopPage from "./components/TopPage";
-import RegisterPage from "./components/RegisterPage";
-import PersonalPage from "./components/PersonalPage";
-import SharePage from "./components/SharePage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import TopPage from "./TopPage";
+import RegisterPage from "./RegisterPage";
+import PersonalPage from "./PersonalPage";
+import SharePage from "./SharePage";
+import Header from "./Header";
+import Footer from "./Footer";
 
 /* ✅ CSS のルート修正 */
 import "./common.css";  
