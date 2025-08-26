071158df7ad07b2ebba333c7e2f914b363398056
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2025-08-26 11:38:24 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./common.css";   // ← 修正！index.css ではなく common.css を読み込む
 
-// React 18 用のレンダリング
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
